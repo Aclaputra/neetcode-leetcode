c8e4818ca658b73464d0c2f217ff2428ca6b53dc
Muhammad Acla <aclalead@gmail.com>
2024-04-04 04:55:25 +0700

    feat: add code valid anagram

--- valid_anagram/solution/bubble_sort/BubbleSort.java
+++ valid_anagram/solution/bubble_sort/BubbleSort.java
@@ -1,6 +1,29 @@
 package valid_anagram.solution.bubble_sort;
 
 public class BubbleSort {
+    public static void bubbleSort(char arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n-1; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    char temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char sArray[] = s.toCharArray();
+        char tArray[] = t.toCharArray();
+
+        bubbleSort(sArray);
+        bubbleSort(tArray);
+
+        return new String(sArray).equals(new String(tArray));
+    }
     public static void main(String[] args) {
         
     }    
--- valid_anagram/solution/sorting_package/SortingPackage.java
+++ valid_anagram/solution/sorting_package/SortingPackage.java
@@ -1,6 +1,19 @@
 package valid_anagram.solution.sorting_package;
 
+import java.util.Arrays;
+
 class SortingPackage {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char sArray[] = s.toCharArray();
+        char tArray[] = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return new String(sArray).equals(new String(tArray));
+    }
     public static void main(String[] args) {
         
     }
